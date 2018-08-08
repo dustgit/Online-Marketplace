428b2f2b7e2ac011d1c3f803a22ca16b914ad5be
Dustin Starke and Kendra Kelly <dustin@>
2018-08-08 16:01:25 -0700

    add in basic js for variables off customer input

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,21 @@
 $(document).ready(function() {
   $("#customerinformation").submit(function(event) {
-    var customerName = $("input#theirN")
+    var customerName = $("input#customerName").val();
+    var address1 = $("input#address1").val();
+    var address2 = $("input#address2").val();
+    var city = $("input#city").val();
+    var state = $("input#state").val();
+    var zip = $("input#zipCode").val();
+    var product = $("input#theirName").val();
+
+    #(".theygohere").text(customerName);
+    #(".theygohere").text(address1);
+    #(".theygohere").text(address2);
+    #(".theygohere").text(city);
+    #(".theygohere").text(state);
+    #(".theygohere").text(zip);
+    #(".theygohere").text(product);
 
     event.preventDefault;
-  })
-
-})
+  });
+});
